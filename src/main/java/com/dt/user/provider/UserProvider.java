9e7684e12dce994924bbe3425824b0e28ed71a87
陈恩惠 <451597529@qq.com>
2018-11-23 17:34:20 +0800

    2018/11/23

--- src/main/java/com/dt/user/provider/UserProvider.java
+++ src/main/java/com/dt/user/provider/UserProvider.java
@@ -87,9 +87,9 @@ public class UserProvider {
             SELECT(" r.r_name,GROUP_CONCAT(DISTINCT u.user_name)as userName,GROUP_CONCAT(DISTINCT m.name)as menuName FROM user_info AS u");
             INNER_JOIN("user_role AS ur ON ur.`u_id`=u.`uid`");
             INNER_JOIN("role AS r ON ur.`r_id`=r.`rid`");
-            INNER_JOIN("role_menu AS rm ON rm.`r_id`=r.`rid`");
-            INNER_JOIN("menu AS m ON m.`menu_id`=rm.`m_id`");
-            GROUP_BY("r.r_name");
+            LEFT_OUTER_JOIN("role_menu AS rm ON rm.`r_id`=r.`rid`");
+            LEFT_OUTER_JOIN("menu AS m ON m.`menu_id`=rm.`m_id`");
+            GROUP_BY("r.rid");
         }}.toString();
     }
 
